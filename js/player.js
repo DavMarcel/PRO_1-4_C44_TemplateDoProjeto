06ef37f3093d3dbc380cb735e59c8970028e490c
DavMarcel <118764668+DavMarcel@users.noreply.github.com>
2023-04-13 14:48:12 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -3,16 +3,15 @@ class Player {
         this.index = null;
         this.distance = 0;
         this.name = null;
-        this.score =0;
-        
-        this.rank = null ;
+        this.score = 0;
+        this.rank = null;
     }
 
     getCount() {
         var playerCountRef = database.ref('playerCount');
         playerCountRef.on("value", (data) => {
             playerCount = data.val();
-        })
+        });
     }
 
     updateCount(count) {
@@ -26,7 +25,7 @@ class Player {
         database.ref(playerIndex).set({
             name: this.name,
             distance: this.distance,
-            score:this.score
+            score: this.score
         });
     }
 
@@ -34,23 +33,18 @@ class Player {
         var playerInfoRef = database.ref('players');
         playerInfoRef.on("value", (data) => {
             allPlayers = data.val();
-        })
+        });
     }
 
-    getPlayerAtEnd(){
-        database.ref('playerAtEnd').on("value",(data)=>{
+    getPlayerAtEnd() {
+        database.ref('playerAtEnd').on("value", (data) => {
             this.rank = data.val();
-        })
+        });
     }
 
-    
-
-    static updatePlayerAtEnd(rank){
-        database.ref("").update({
-            playerAtEnd : 0
-        })
+    static updatePlayerAtEnd(rank) {
+        database.ref('').update({
+            playerAtEnd: rank
+        });
     }
-
-
-    
 }
